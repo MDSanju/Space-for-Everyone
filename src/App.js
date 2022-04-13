fc8bec0d002ce322d53017fa4cf326fb2d3be0d0
MD Sanju <mdsanju387@gmail.com>
2022-04-13 14:09:37 +0600

    Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Story from "./Pages/Story/Story";
+import Blog from "./Pages/Blog/Blog";
+import Contact from "./Pages/Contact/Contact";
+import NoMatch from "./Pages/NoMatch/NoMatch";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h2>Hello World!</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/story" element={<Story />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
